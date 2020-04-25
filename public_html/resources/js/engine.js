611e8bb8466b4650ee85819030df8b559b9b1f14
petrh <petrh@192.168.0.234>
2020-04-25 09:31:01 +0200

    zacatky js css html

---
+++ public_html/resources/js/engine.js
@@ -0,0 +1,30 @@
+
+
+window.onload = function ()
+{
+  init();
+};
+
+let prvniHracNaTahu = true;
+
+function init() {
+  
+}
+
+let pole1 = {clicked:false, player:0};
+let pole2 = {clicked:false, player:0};
+let pole3 = {clicked:false, player:0};
+let pole4 = {clicked:false, player:0};
+let pole5 = {clicked:false, player:0};
+let pole6 = {clicked:false, player:0};
+let pole7 = {clicked:false, player:0};
+let pole8 = {clicked:false, player:0};
+let pole9 = {clicked:false, player:0};
+
+function poleClick(poleCislo) {
+  console.log("pole=" + poleCislo);
+}
+
+
+
+
