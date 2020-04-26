8029ea6b5752512bf5871ee05f539adfdb9cd18d
petrh <petrh@192.168.0.234>
2020-04-26 14:01:48 +0200

    js engine first version

--- public_html/resources/js/engine.js
+++ public_html/resources/js/engine.js
@@ -1,30 +1,66 @@
-
-
-window.onload = function ()
-{
-  init();
-};
+//window.onload = function ()
+//{
+//  init();
+//};
+//
+//function init() {
+//  
+//}
 
 let prvniHracNaTahu = true;
+let pole1 = {id:1, clicked:false, player:0, class:''};
+let pole2 = {id:2, clicked:false, player:0, class:''};
+let pole3 = {id:3, clicked:false, player:0, class:''};
+let pole4 = {id:4, clicked:false, player:0, class:''};
+let pole5 = {id:5, clicked:false, player:0, class:''};
+let pole6 = {id:6, clicked:false, player:0, class:''};
+let pole7 = {id:7, clicked:false, player:0, class:''};
+let pole8 = {id:8, clicked:false, player:0, class:''};
+let pole9 = {id:9, clicked:false, player:0, class:''};
+var poleAll = [pole1, pole2, pole3, pole4, pole5, pole6, pole7, pole8, pole9];
 
-function init() {
+function poleClick(poleCislo) {
+  poleAll.forEach(function(pole) {
+    if (poleCislo == pole.id) {
+      if (!pole.clicked) {
+        pole.clicked = true;
+        if (prvniHracNaTahu) {
+          pole.player = 1;
+          pole.class = 'crossBG';
+          prvniHracNaTahu = !prvniHracNaTahu;
+        } else {
+          pole.player = 2;
+          pole.class = 'circleBG';
+          prvniHracNaTahu = !prvniHracNaTahu;
+        }
+        render();
+      }
+      return;
+    }
+  });
+  
+}
+function render() {
+  poleAll.forEach(function(pole) {
+    document.getElementById('pole'+pole.id).className = 'pole ' + pole.class;
+  });
+  document.getElementById('playersIcon').className = prvniHracNaTahu ? 'crossBG' : 'circleBG';
+}
+
+function zjistiVyhru() {
   
 }
 
-let pole1 = {clicked:false, player:0};
-let pole2 = {clicked:false, player:0};
-let pole3 = {clicked:false, player:0};
-let pole4 = {clicked:false, player:0};
-let pole5 = {clicked:false, player:0};
-let pole6 = {clicked:false, player:0};
-let pole7 = {clicked:false, player:0};
-let pole8 = {clicked:false, player:0};
-let pole9 = {clicked:false, player:0};
-
-function poleClick(poleCislo) {
-  console.log("pole=" + poleCislo);
+function reset() {
+  poleAll.forEach(function(pole) {
+    pole.player = 0;
+    pole.clicked = false;
+    pole.class = '';
+  });
+  prvniHracNaTahu = true;
+  render();
 }
 
-
-
-
+function vypisPole() {
+  console.log(poleAll);
+}
